6e3d408b70e94f703545f9385bc3644e61331249
zhan2016 <ronzhang2015@gmail.com>
2023-04-25 11:45:10 +0800

    fix something

---
+++ gn/skia/lowest_version_dir.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import re
+import sys
+
+dirpath = sys.argv[1]
+regex = re.compile(sys.argv[2])
+minimum = sys.argv[3]
+
+matches = sorted(filter(regex.match, os.listdir(dirpath)))
+for match in matches:
+    if (match >= minimum):
+        path = os.path.join(dirpath, match)
+        if os.path.isdir(path):
+            print(match)
+            break
